67f49b3de703bde1c13b34f882297e86892d888b
Mark Carver <mark.carver@me.com>
2018-02-26 12:51:25 -0600

    Issue #2876808 by markcarver, paul.kim: Refactor composer_manager_build_json() to be more dynamic

--- composer_manager.api.php
+++ composer_manager.api.php
@@ -21,6 +21,47 @@ function hook_composer_json_alter(&$json) {
 }
 
 /**
+ * Allow modules to alter the JSON mappings.
+ *
+ * @param array $map
+ *   An associative array of key/value pairs, containing:
+ *   - 'properties': (string[]|array[]) An indexed array of whitelisted
+ *     property strings to be merged into the compiled Composer JSON file. If
+ *     the specified property is an array, it will be treated a "parents" array
+ *     to retrieve a nested value, see drupal_array_get_nested_value().
+ *   - 'relative_paths': (array) An associative array of key/value pairs,
+ *     containing:
+ *     - 'keys': (string[]|array[]) An indexed array of property strings that
+ *       will be iterated over to transform its keys into relative paths. If
+ *       the specified property is an array, it will be treated a "parents"
+ *       array to retrieve a nested value, see drupal_array_get_nested_value().
+ *     - 'values': (string[]|array[]) An indexed array of property strings that
+ *       will be iterated over to transform its values into relative paths. If
+ *       the specified property is an array, it will be treated a "parents"
+ *       array to retrieve a nested value, see drupal_array_get_nested_value().
+ *
+ * @see composer_manager_build_json()
+ */
+function hook_composer_json_map_alter(array &$map) {
+  // NOTE: the following code is just for example. These values are already
+  // added to the JSON map by default and do not need to be specified again.
+
+  // Whitelist a specific top level property.
+  $map['properties'][] = 'config';
+
+  // Whitelist a specific sub-property (e.g. not the whole "extra" property).
+  $map['properties'][] = array('extra', 'installer-paths');
+
+  // Let composer manager know that a specific property keys are paths and
+  // should be converted into relative paths from the generated Composer JSON.
+  $map['relative_paths']['keys'][] = array('extra', 'installer-paths');
+
+  // Let composer manager know that a specific property values are paths and
+  // should be converted into relative paths from the generated Composer JSON.
+  $map['relative_paths']['values'][] = array('extra', 'patches');
+}
+
+/**
  * Allow modules to perform tasks after a composer install has been completed.
  */
 function hook_composer_dependencies_install() {
